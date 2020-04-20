3e33fe8d83a532e74a72152fe9ee402d37925dd1
Nick <Pedalino>
2020-04-20 00:12:48 -0400

    pixel grid moved to a series of scaled textures

--- Unity/CellularAutomataCustom/Assets/src/TileGridBehavior.cs
+++ Unity/CellularAutomataCustom/Assets/src/TileGridBehavior.cs
@@ -6,78 +6,86 @@ using UnityEngine.UI;
 
 namespace ca
 {
-    [RequireComponent(typeof(GridLayoutGroup))]
     public class TileGridBehavior : MonoBehaviour
     {
-        private GridLayoutGroup m_Grid;
-        private RectTransform m_RectTransform;
-        private Dictionary<Vector2Int, TileBehavior> m_Tiles = new Dictionary<Vector2Int, TileBehavior>();
-
         [Sirenix.OdinInspector.Required]
-        public GameObject m_TilePrefab = null;
+        public RectTransform m_RectTransform;
+        //private Dictionary<Vector2Int, TileBehavior> m_Tiles = new Dictionary<Vector2Int, TileBehavior>();
 
-        #region Width/Height
-        //width & height in # of tiles
-        [Range(2,1024)]
-        public int m_TileSize;
-        #endregion
+        private class ZoomLevel
+        {
+            public Texture2D m_Tex = null;
+            public int m_Size = 0;
+            public ZoomLevel(int size) { m_Size = size; m_Tex = null; }
+        }
+
+
+        //s_Textures Index, pixel width
+        private Dictionary<int, ZoomLevel> m_ZoomLevels = new Dictionary<int, ZoomLevel>
+        {
+            {0, new ZoomLevel(4)},
+            {1, new ZoomLevel(8)},
+            {2, new ZoomLevel(16)},
+            {3, new ZoomLevel(32)},
+            {4, new ZoomLevel(64)},
+            {5, new ZoomLevel(128)},
+            {6, new ZoomLevel(256)},
+            {7, new ZoomLevel(512)},
+            {8, new ZoomLevel(1024)}
+        };
+        private const int MAX_ZOOM = 8;
+        [Range(0, MAX_ZOOM)]
+        public int m_Zoom = 0;
+
+        [Sirenix.OdinInspector.Button(Sirenix.OdinInspector.ButtonSizes.Medium, Name = "Zoom In")]
+        private void ZoomIn()
+        {
+            m_Zoom = Math.Max(0, m_Zoom - 1);
+
+            GetComponent<RawImage>().texture = m_ZoomLevels[m_Zoom].m_Tex;
+        }
+        [Sirenix.OdinInspector.Button(Sirenix.OdinInspector.ButtonSizes.Medium, Name = "Zoom Out")]
+        private void ZoomOut()
+        {
+            m_Zoom = Math.Min(MAX_ZOOM, m_Zoom + 1);
+
+            GetComponent<RawImage>().texture = m_ZoomLevels[m_Zoom].m_Tex;
+        }
 
         [Sirenix.OdinInspector.Button(Sirenix.OdinInspector.ButtonSizes.Medium, Name = "Refresh Grid")]
         private void ResetGrid()
         {
-            if (m_Tiles == null)
+            for (int i = 0; i < MAX_ZOOM + 1; i++)
             {
-                m_Tiles = new Dictionary<Vector2Int, TileBehavior>();
+                m_ZoomLevels[i].m_Tex = null;
             }
-            if (m_Grid == null)
-            {
-                m_Grid = GetComponent<GridLayoutGroup>();
-            }
-            if (m_RectTransform == null)
-            {
-                m_RectTransform = GetComponent<RectTransform>();
-            }
-            DestroyTiles();
-            InitializeTiles();
+            InitializeTextures();
         }
 
-        private void DestroyTiles()
+        private void InitializeTextures()
         {
-            transform.DestroyAllChildren();
-            m_Tiles.Clear();
-            StartCoroutine(WaitAFrame());
-        }
-        IEnumerator WaitAFrame()
-        {
-            yield return 0;
-        }
-
-        private void InitializeTiles()
-        {
-            //set size of each cell as ratio of (single tile size) / (# tiles)
-            m_Grid.cellSize = new Vector2(
-                m_RectTransform.rect.width / (float)m_TileSize,
-                m_RectTransform.rect.height / (float)m_TileSize
-            );
-
-            for (int y = 0; y < m_TileSize; y++)
+            //for each zoom level, construct a unique texture and store it in a collection
+            for (int i = 0; i < MAX_ZOOM+1; i++)
             {
-                for (int x = 0; x < m_TileSize; x++)
+                //create a square texture, sizes defined in zoom levels dictionary
+                m_ZoomLevels[i].m_Tex = new Texture2D(m_ZoomLevels[i].m_Size, m_ZoomLevels[i].m_Size);
+
+                //initialize texture to white
+                for (int y = 0; y < m_ZoomLevels[i].m_Tex.height; y++)
                 {
-                    GameObject go = Instantiate(m_TilePrefab);
-                    Vector2Int index = new Vector2Int(x, y);
-                    m_Tiles[index] = go.GetComponent<TileBehavior>();
-                    go.transform.SetParent(transform);
+                    for (int x = 0; x < m_ZoomLevels[i].m_Tex.width; x++)
+                    {
+                        m_ZoomLevels[i].m_Tex.SetPixel(x, y, x % 2 == 1 ? Color.white : Color.black);
+                    }
                 }
+                m_ZoomLevels[i].m_Tex.Apply();
+                m_ZoomLevels[i].m_Tex.filterMode = FilterMode.Point;
             }
         }
-
-
 
         public void Awake()
         {
-            m_Grid = GetComponent<GridLayoutGroup>();
-            Debug.Assert(m_Grid != null);
+
         }
 
 
