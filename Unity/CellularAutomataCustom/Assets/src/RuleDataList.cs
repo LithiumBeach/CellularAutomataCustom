9b8f47967afdce45603a8878e2df736110287756
Nick <Pedalino>
2021-01-05 20:49:54 -0700

    Adds List wrappers for serializing List<List<RuleData>> (rulesets)

---
+++ Unity/CellularAutomataCustom/Assets/src/RuleDataList.cs
@@ -0,0 +1,15 @@
+﻿//RuleDataList.cs
+//Serializable List<RuleDatas>
+using System.Collections.Generic;
+
+namespace ca
+{
+    [System.Serializable]
+    public class RuleDataList
+    {
+        public RuleDataList() { list = new List<RuleData>(); }
+        public RuleDataList(List<RuleData> l) { list = new List<RuleData>(l); }
+
+        public List<RuleData> list;
+    }
+}
---
+++ Unity/CellularAutomataCustom/Assets/src/RulesetList.cs
@@ -0,0 +1,21 @@
+﻿//RulesetList.cs
+//Serializable List<List<RuleData>
+using System.Collections.Generic;
+
+namespace ca
+{
+    [System.Serializable]
+    public class RulesetList
+    {
+        public RulesetList() { list = new List<RuleDataList>(); }
+        public RulesetList(List<RulesetSO> rso)
+        {
+            list = new List<RuleDataList>();
+            foreach (var ruleset in rso)
+            {
+                list.Add(new RuleDataList(ruleset.m_Rules));
+            }
+        }
+        public List<RuleDataList> list;
+    }
+}
